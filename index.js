be20ef8b7b861a3e886fe5096c381e9090c0f00c
jonas-45 <jonaswullah@gmail.com>
2023-01-02 15:34:43 +0000

    show mobile menu when hamburger icon is clicked

--- index.js
+++ index.js
@@ -2,14 +2,33 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
 const logo = document.querySelector('.logo');
+const menu = document.querySelector(".pop-menu ul");
 
 hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-  logo.classList.toggle('active');
+  // hamburger.classList.toggle('active');
+  // navMenu.classList.toggle('active');
+  // logo.classList.toggle('active');
+  showMobileMenu();
 });
 
 document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navMenu.classList.remove('active');
 }));
+
+function showMobileMenu(){
+  //alert("why are you not working?");
+  let bg = "./images/overlay.png";
+  let navContainer = document.querySelector(".nav-container");
+  let dnav = document.querySelector(".desktop-nav");
+  let navbar = document.querySelector(".navbar");
+  const lastMenuItem = document.querySelector(".last-menu-item");
+
+  navContainer.style.backgroundImage = "url(" + bg + ")";
+
+  navContainer.style.display = "block";
+  dnav.style.display = "flex";
+  navbar.style.display = "none";
+  lastMenuItem.style.display = "none";
+  navContainer.classList.add("displayMenu");
+}
