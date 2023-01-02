cd65f8dca1ce5860a2052eba7c3bd8454c5f7eae
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-02 21:03:23 +0100

    Add cancel bar

--- index.js
+++ index.js
@@ -1,32 +1,23 @@
 // hambuger
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
-
-hamburger.addEventListener('click', () => {
-  // hamburger.classList.toggle('active');
-  // navMenu.classList.toggle('active');
-  // logo.classList.toggle('active');
-  showMobileMenu();
-});
-
-document.querySelectorAll('.link').forEach((n) => n.addEventListener('click', () => {
-  hamburger.classList.remove('active');
-  navMenu.classList.remove('active');
-}));
 
 function showMobileMenu() {
-  // alert("why are you not working?");
   const bg = './images/overlay.png';
   const navContainer = document.querySelector('.nav-container');
-  const dnav = document.querySelector('.desktop-nav');
+  const desktop = document.querySelector('.desktop-nav');
   const navbar = document.querySelector('.navbar');
   const lastMenuItem = document.querySelector('.last-menu-item');
+  const cancel = document.querySelector('.cancel');
 
   navContainer.style.backgroundImage = `url(${bg})`;
-
+  cancel.style.display = 'block';
   navContainer.style.display = 'block';
-  dnav.style.display = 'flex';
+  desktop.style.display = 'flex';
   navbar.style.display = 'none';
   lastMenuItem.style.display = 'none';
   navContainer.classList.add('displayMenu');
 }
+
+hamburger.addEventListener('click', () => {
+  showMobileMenu();
+});
