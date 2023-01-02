a94586bd6acea0de5b25548c1a4ef0926a33e0dc
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-02 15:17:02 +0100

    Activated hamburger icon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // hambuger
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+const navbar = document.querySelector('.logo');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
