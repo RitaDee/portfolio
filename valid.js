4c9365d741d98fa79086769848358c4e769a978f
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-01-07 18:29:07 +0100

    fixed bug

--- valid.js
+++ valid.js
@@ -10,6 +10,7 @@ function formValidate(event) {
   if (email.value !== email.value.toLowerCase()) {
     error.style.visibility = 'visible';
     error.innerHTML = 'Please input your email address in lowercase.';
+    event.preventDefault();
   } else {
     error.style.visibility = 'hidden';
   }
